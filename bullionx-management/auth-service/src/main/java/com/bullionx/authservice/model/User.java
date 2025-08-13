d7dc5711ad04312b5c4a365edbd3814e551d0d16
Pranav B <bhoopal.p@northeastern.edu>
2025-08-13 14:52:54 -0400

    Created User Model and Reposiroty

--- bullionx-management/auth-service/src/main/java/com/bullionx/authservice/model/User.java
+++ bullionx-management/auth-service/src/main/java/com/bullionx/authservice/model/User.java
@@ -1,4 +1,37 @@
 package com.bullionx.authservice.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.UUID;
+
+@Entity(name = "UserTable")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @UuidGenerator
+    private UUID userId;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Column(unique = true)
+    @Email
+    private String email;
+
+
+    private String password;
 }
---
+++ bullionx-management/auth-service/src/main/java/com/bullionx/authservice/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.bullionx.authservice.repository;
+
+public interface UserRepository {
+}
